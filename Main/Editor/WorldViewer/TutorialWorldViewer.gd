abcab757f2699d4a8ff7d0b671e82cd64c028040
Negabinary <negabinary@gmail.com>
2019-12-22 14:39:28 +0000

    HTML Export

--- Main/Editor/WorldViewer/TutorialWorldViewer.gd
+++ Main/Editor/WorldViewer/TutorialWorldViewer.gd
@@ -18,9 +18,14 @@ func update_previews(preview=null):
 
 func set_picture(time_state:TimeState) -> void:
 	var time := time_state.get_time()
-	if time == 0:
-		$A.show(); $B.hide(); $C.hide();
-	elif time % 2 == 1:
-		$A.hide(); $B.show(); $C.hide();
+	if time % 2 == 0:
+		$A.show(); $B.hide()
 	else:
-		$A.hide(); $B.hide(); $C.show();
+		$B.show(); $A.hide()
+	
+	while $C.get_child_count() < floor(time / 2):
+		print("Hey!")
+		var d = $D.duplicate()
+		$C.add_child(d)
+		d.rect_position = Vector2(randf() * ($C.rect_size.x - d.rect_size.x), 0)
+		d.show()
